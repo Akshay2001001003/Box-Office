08c22b2026913d81c74d143ea25840a7f0cc1d86
akshay <kumarakshay6979@gmail.com>
2022-08-08 08:58:33 +0530

    this is props

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-
+import Board from './components/Board.jsx';
 export default () => (
   <div>
     <h1>TIC TAC TOE</h1>
+    <Board />
   </div>
 );
--- my-app/src/components/Board.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-export const board = () => {
-  return <div>board</div>;
-};
-export default Board;
